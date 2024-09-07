8a8b6e0070eb9472f7e3d688ef946def635bf1d1
william <william.frazao91@gmail.com>
2024-09-07 15:04:30 -0300

    adicionando o userContext no projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './Components/Header';
-import Home from './Components/Home';
-import Login from './Components/Login/Login';
-import Footer from './Components/Footer';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+import { UserStorage } from "./UserContext";
+import Footer from "./Components/Footer";
 
 const App = () => {
   return (
-  <div>
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login/*' element={<Login />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  </div>
+    <div>
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
+    </div>
   );
 };
 
