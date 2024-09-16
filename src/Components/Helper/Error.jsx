46ca03daf95ad754d7d2f55b59d892f145b68978
william <william.frazao91@gmail.com>
2024-09-16 15:42:41 -0300

    estilazacao da tela de login

---
+++ src/Components/Helper/Error.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Error = ({ error }) => {
+  if (!error) return null;
+  return (
+    <p style={{color: '#f31', margin: '1rem 0'}}>Error</p>
+  )
+};
+
+export default Error;
