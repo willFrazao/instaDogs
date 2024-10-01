4d19bcb75e4e238cf6d9c86d70c05e5577a422bb
william <william.frazao91@gmail.com>
2024-09-30 21:58:35 -0300

    Protected Route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
+import User from './Components/User/user';
+import ProtectedRoute from './Components/Helper/ProtectedRoute';
 import { UserStorage } from './UserContext';
 
 function App() {
@@ -15,7 +17,15 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <Route 
+              path="conta/*" 
+              element={
+                <ProtectedRoute>
+                  <User/>
+                </ProtectedRoute>
+              } 
+              />
           </Routes>
           <Footer />
         </UserStorage>
