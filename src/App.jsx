74cfe6854a02ff32281fc001d664ded067068c7a
william <william.frazao91@gmail.com>
2024-11-12 11:00:09 -0300

    DONE: Foto unica

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Login from './Components/Login/Login';
 import User from './Components/User/user';
 import ProtectedRoute from './Components/Helper/ProtectedRoute';
 import { UserStorage } from './UserContext';
+import Photo from './Components/Photo/Photo';
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
                 </ProtectedRoute>
               } 
               />
+              <Route path="foto/:id" element={<Photo />} />
           </Routes>
           <Footer />
         </UserStorage>
--- src/Components/Photo/PhotoComments.jsx
+++ src/Components/Photo/PhotoComments.jsx
@@ -14,7 +14,11 @@ const PhotoComments = (props) => {
 
   return (
     <>
-    <ul ref={commentsSection} className={styles.comments}>
+    <ul 
+      ref={commentsSection} 
+      className={`${styles.comments} ${props.single ? styles.single : ''}
+      `}
+    >
       {comments.map((comment) => (
         <li key={comment.comment_ID}>
           <b>{comment.comment_author}: </b>
@@ -23,7 +27,7 @@ const PhotoComments = (props) => {
       ))}
 
     </ul>
-      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+      {login && <PhotoCommentsForm single={props.single} id={props.id} setComments={setComments} />}
     </>
   );
 };
