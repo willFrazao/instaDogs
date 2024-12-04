5c3523a11ec5fa0559471d1568f70dde4ae8429b
william <william.frazao91@gmail.com>
2024-12-04 09:58:58 -0300

    usando a biblioteca externa no projeto

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import Head from '../Helper/Head';
+import useFetch from '../../Hooks/useFetch';
+import { STATS_GET } from '../../api';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Error'
+import UserStatsGraphs from './UserStatsGraphs';
 
 const UserStats = () => {
+  const {data, error, loading, request} = useFetch();
+
+React.useEffect(() => {
+  async function getData() {
+    const {url, options} = STATS_GET();
+    await request(url, options)
+  }
+  getData();
+}, [request]);
+
+if (loading) return <Loading />
+if (error) return <Error error={error} />
+if (data)
   return (
     <div>
       <Head title="Estatisticas" />
-      Estatisticas
+      <UserStatsGraphs data={data} />
     </div>
   )
 }
